6f09644e7536445ebb62e1231918baf262cacf10
Agraulis <72881808+Agraulis@users.noreply.github.com>
2023-01-26 03:26:33 +0500

    Delete login.py

--- instagram_scraping/login.py
+++
@@ -1,3 +0,0 @@
-USERNAME = 'Agraulisoris'
-PASSWORD = '#PWD_INSTAGRAM_BROWSER:10:1674652138:AfRQAMGf12hE0cjGfF786yy1DTNRkYoqZOooM/egmALJFzlR68OGCkyzuQUW0Y/szYzpKHy+Q86VMSe2/FpmDmR9p7Yz39IY5g1eqMudbegTo+40acg6T5+bVk3mUf0JgXmrquljBDpQNVqftfZudMNqXOP0'
-insta_users = ('mercool.andrey', 'dmitriikovalenko246')
